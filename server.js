492aac7a19939cb0197d403b0951a6bddc9312a0
Ricardo Fanchin <fanchinricardo@gmail.com>
2025-11-11 23:34:13 -0300

    build4

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/api/locations/latest', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Se não encontrar rota, devolve index.html (React Router)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
